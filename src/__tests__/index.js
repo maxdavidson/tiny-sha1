b95c59e4bd10f9bee7f5cf0ca186949996e80186
Max Davidson <davidsonmax@gmail.com>
2016-09-26 00:03:25 +0200

    Refactor tests to use Node buffers

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,20 +1,20 @@
 import test from 'ava';
 import sha1 from '..';
-import { stringToBuffer, nativeSha1, createRandomUint8Array } from './_helpers';
+import { nativeSha1, randomBytes } from './_helpers';
 
 test('Invalid data should throw type error', t => {
   t.throws(() => sha1('hello there'), TypeError);
 });
 
 test('Sample text', t => {
-  const buffer = stringToBuffer('The quick brown fox jumps over the lazy dog');
+  const buffer = Buffer.from('The quick brown fox jumps over the lazy dog');
   t.is(sha1(buffer), nativeSha1(buffer));
 });
 
 for (let i = 0; i < 50; i++) {
   const length = Math.pow(i, 2.5) | 0;
   test(`Random data of length: ${length}`, async t => {
-    const buffer = await createRandomUint8Array(length);
+    const buffer = await randomBytes(length);
     t.is(sha1(buffer), nativeSha1(buffer));
   });
 }
