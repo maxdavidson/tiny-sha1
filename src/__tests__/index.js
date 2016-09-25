e4443fe9e7dae172ee34c96926430fba2e63f8e4
Max Davidson <davidsonmax@gmail.com>
2016-09-26 00:08:09 +0200

    Add more tests to reach 100% coverage

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import sha1 from '..';
+import { getUint8Array } from '../utils';
 import { nativeSha1, randomBytes } from './_helpers';
 
 test('Invalid data should throw type error', t => {
@@ -9,6 +10,18 @@ test('Invalid data should throw type error', t => {
 test('Sample text', t => {
   const buffer = Buffer.from('The quick brown fox jumps over the lazy dog');
   t.is(sha1(buffer), nativeSha1(buffer));
+});
+
+test('getUint8Array', t => {
+  t.true(getUint8Array(new Uint8Array(1024)) instanceof Uint8Array);
+  t.true(getUint8Array(new ArrayBuffer(1024)) instanceof Uint8Array);
+  t.true(getUint8Array(Buffer.alloc(1024)) instanceof Uint8Array);
+  t.true(getUint8Array(new Uint32Array(256)) instanceof Uint8Array);
+  t.true(getUint8Array(new Float64Array(128)) instanceof Uint8Array);
+
+  t.throws(() => getUint8Array());
+  t.throws(() => getUint8Array('hello'));
+  t.throws(() => getUint8Array(42));
 });
 
 for (let i = 0; i < 50; i++) {
